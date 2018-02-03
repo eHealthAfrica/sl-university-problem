10681789af751224097ad6a3d871106c5898f878
JasperTimm <jasper.timm@ehealthnigeria.org>
2018-02-03 13:14:36 +0000

    Remove posts.js for problem

--- app/scripts/post.js
+++ app/scripts/post.js
@@ -2,57 +2,21 @@
 class Post{
   constructor(){
     this.posts = [];
-    this.API = 'https://6347ecb1.ngrok.io';
+    this.API = 'https://127.0.0.1:5984';
 
     this.refreshPostList();
   }
 
   savePostInAPI(post) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', this.API + '/posts', false);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(post));
+    //Send post to the API via XHR (AJAX)
   }
 
   refreshPostList() {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', this.API + '/posts/_all_docs?include_docs=true', false);
-    xhr.send(null);
-
-    let resp = JSON.parse(xhr.responseText);
-
-    this.posts = [];
-    for (let i = 0; i < resp.rows.length ; i++) {
-      let doc = resp.rows[i];
-      let newPost = {
-        title: doc.doc.title,
-        body: doc.doc.body,
-        lastModified: doc.doc.lastModified,
-        id: i
-      }
-
-      console.log(newPost);
-
-      this.posts.push(newPost);
-    }
-
-    console.log("Done refreshing!");
-    console.log(this.posts);
+    //Get all the posts from the API and return them
   }
 
   savePost(title, body) {  // ajax
-    let post = {
-      title: title,
-      body: body,
-      lastModified: registration.user
-    }
-
-    this.posts.push(post);
-    this.savePostInAPI(post);
-    console.log('Saved!')
-    this.refreshPostList();
-    console.log('Refreshed!');
-    events.trigger(eventNames.saved, post);
+    //Save one post, called from outside
   }
 
   getPost(id) {  // ajax
